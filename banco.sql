2077f0a5aaa60510cad43923a3598d596aba0f87
Lhaisa <lhaisa.macedo@escola.pr.gov.br>
2026-03-25 16:50:16 -0300

    Add files via upload

---
+++ banco.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE produ;
+USE produ;
+
+CREATE TABLE produtos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    categoria VARCHAR(100) NOT NULL,
+    preco DECIMAL (10,2) NOT NULL,
+    quantidade INT NOT NULL
+);
+
